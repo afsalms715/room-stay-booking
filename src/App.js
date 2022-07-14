a923f40d538c3c893bb08e906558bf2bc07c079e
afsalms715 <afsalms715@gamil.com>
2022-07-14 22:36:57 +0300

    login-page-styling
    Signed-off-by: afsalms715 <afsalms715@gamil.com>

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import roomstaylogo from './roomstaylogo.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="wraper">
+        <div className="content">
+          <div className="Logo">
+            <img src={roomstaylogo} alt="" />
+          </div>
+          <div className="cl">
+            <span>Become an member of HomeStay today</span>
+          </div>
+          <div className="form">
+            <div className="status"> 
+                test 
+            </div>
+            <form action='#'>
+              <div className="eInput">
+                <FontAwesomeIcon className='icon' icon={faUser}/>
+                <input type="text" id="input1" placeholder='Enter Your Email Address' />
+              </div>
+              <div className="eInput">
+                <FontAwesomeIcon className='icon' icon={faLock}/>
+                <input type="password" id="input2" placeholder='Enter Your Password' />
+              </div>
+              <div className="submit">
+                <button type='submit'>Submit</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
